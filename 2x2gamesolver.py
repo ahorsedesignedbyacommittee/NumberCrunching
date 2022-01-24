15746e409410db51dd930c6395cd7d24601a30b3
ahorsedesignedbyacommittee <80117449+ahorsedesignedbyacommittee@users.noreply.github.com>
2022-01-24 19:05:06 +0100

    Update 2x2gamesolver.py

--- 2x2gamesolver.py
+++ 2x2gamesolver.py
@@ -1,9 +1,9 @@
 import tkinter as tk
 import tkinter.messagebox
-from numpy import array
+from functools import partial
 
-#----------------------- Setting up GUI --------------------------------
-#Setting up matrix
+# ----------------------- Setting up GUI --------------------------------
+# Setting up home screen
 window = tk.Tk()
 window.title("2x2 simultaneous game solver")
 canvas = tk.Canvas()
@@ -40,147 +40,89 @@ dr1entry.place(x = 250, y = 290)
 dr2entry = tk.Entry(width=6)
 dr2entry.place(x = 280, y = 260)
 
-#Importing images
+# Importing images
 weak_dominance_horizontal = tk.PhotoImage(file = 'weak_dominance_horizontal.gif')
 strict_dominance_horizontal = tk.PhotoImage(file = 'strict_dominance_horizontal.gif')
 weak_dominance_vertical = tk.PhotoImage(file = 'weak_dominance_vertical.gif')
 strict_dominance_vertical = tk.PhotoImage(file = 'strict_dominance_vertical.gif')
 
-def get_row_payoffs():
-    #Obtains the row player's payoff matrix from the entry fields
-    return [float(ul1entry.get()), float(ur1entry.get()), 
-    float(dl1entry.get()), float(dr1entry.get())]
+# One global variable that will become of relevance for oddment calculations (which do not work in presence of strict dominance)
+strict_dominance_exists = False
 
-#-------------------- Prepopulating entry fields------------------------
 
-#These two functions facilitate the population of the entry fields for
-#the column player payoffs. When insert_zero_sum_payoffs() populates
-#the entry field for the column player in each cell with the negative
-#of the row player'S payoff in that cell; insert_constant_sum_payoffs()
-#populates it with the remainder so that the payoffs of the two players
-#in each cell reach a constant sum != 0. In the latter case, the constant
-#sum must be provided in the dedicated field. Row player payoffs must always
-#be provided. Manual overwriting of the prepopulated entries is possible.
+# ------------- Generating payoff matrixes for both players -------------
 
-    
-def insert_zero_sum_payoffs():
-    try:
-        row_payoffs = get_row_payoffs()
-        ul2entry.insert(0, -row_payoffs[0])
-        ur2entry.insert(0, -row_payoffs[1])
-        dl2entry.insert(0, -row_payoffs[2])
-        dr2entry.insert(0, -row_payoffs[3])
-    except ValueError:
-        warning1 = tkinter.messagebox.showinfo(message="Please fill in all Row player fields.")
-    return
-    
 def insert_constant_sum_payoffs():
-    try:
-        row_payoffs = get_row_payoffs()
+    if constantsumentry.get() == "" or constantsumentry.get() == None:
+        sum = 0
+    else:
         sum = float(constantsumentry.get())
-        ul2entry.insert(0, sum-float(row_payoffs[0]))
-        ur2entry.insert(0, sum-float(row_payoffs[1]))
-        dl2entry.insert(0, sum-float(row_payoffs[2]))
-        dr2entry.insert(0, sum-float(row_payoffs[3]))
+    map_dict = {ul2entry:ul1entry, ur2entry:ur1entry, dl2entry:dl1entry, dr2entry:dr1entry}
+    try:
+        for column_entry_field in map_dict:
+            new_value = sum - float(map_dict[column_entry_field].get())
+            # The following block is to nicely display inserted values wihtout decimals if they are integers. For the calculations later on, all payoffs will be entered as floats into the payoff matrix
+            if new_value.is_integer():
+                new_value = int(new_value)  
+            column_entry_field.insert(0, new_value)
     except ValueError:
         warning2 = tkinter.messagebox.showinfo(message="Please fill in all Row player fields.")
-    return
     
-zerosumbutton = tk.Button(text="Zero sum", command=insert_zero_sum_payoffs, width=10)
-zerosumbutton.place(x = 20, y = 400)
 
-constantsumbutton = tk.Button(text="Constant sum", command=insert_constant_sum_payoffs, width=10)
-constantsumbutton.place(x = 110, y = 400)
-
-constantsumentry = tk.Entry(width = 10)
-constantsumentry.place(x = 110, y = 430)
-
-
-#------------- Generating payoff matrixes for both players -------------
 def get_payoffs():
+    # Returns payoff matrix as a list in form: 
+    # [ 
+    #       [[Row-up-left, Row-up-right], [Row-down-left, Row-down-right]],
+    #       [[Column-up-left, Column-up-right], [Column-down-left, Column-down-right]]
+    # ]
     try:
-        p_row = array([
-            [float(ul1entry.get()), float(dl1entry.get())],
-            [float(ur1entry.get()), float(dr1entry.get())],
-            ])
-        p_column = array([
-            [float(ul2entry.get()), float(dl2entry.get())],
-            [float(ur2entry.get()), float(dr2entry.get())],
-            ])    
-        return [p_row, p_column]
-    except ValueError:
-        msgwrng3 = "Please ensure all fields are filled before proceeding."
-        warning3 = tkinter.messagebox.showinfo(message=msgwrng3)
-    
-def dom_checker(p_row, p_column):
-    #This function runs dominance (weak or strict) tests for both players
-    #The output is dic_of_dominance where each of the four strategies U/D and L/R
-    #is coded based on whether it is dominant:
-    #0 = no dominance, 1 = weak dominance, 2 = strict dominance
-    dic = {"u":0, "d":0, "l":0, "r":0}
-    if (p_row[0,:]>p_row[1,:]).any() and (p_row[0,:]>=p_row[1,:]).all():
-        dic["u"] = 1
-    if (p_row[0,:]>p_row[1,:]).all():
-        dic["u"] = 2
-    if (p_row[1,:]>p_row[0,:]).any() and (p_row[1,:]>=p_row[0,:]).all():
-        dic["d"] = 1
-    if (p_row[1,:]>p_row[0,:]).all():
-        dic["d"] = 2
-    if (p_column[:,0]>p_column[:,1]).any() and (p_column[:,0]>=p_column[:,1]).all():
-        dic["l"] = 1
-    if (p_column[:,0]>p_column[:,1]).all():
-        dic["l"] = 2
-    if (p_column[:,1]>p_column[:,0]).any() and (p_column[:,1]>=p_column[:,0]).all():
-        dic["r"] = 1
-    if (p_column[:,1]>p_column[:,0]).all():
-        dic["r"] = 2
-    return dic
-    
-#----------- Checking for pure and mixed strategy equilibria -----------
-
-def pure_equil_checker(p_row, p_column):
-    #This function returns a dictionary of the four possible cells 
-    #(outcomes) in pure strategies (00 = U/L, 01 = D/L, 10 = UR, 11 = D/R)
-    #The value for each cell that is returned will be True if the cell 
-    #is a Nash equilibrium, False otherwise
-    dic = {"00": False, "01":False, "10":False, "11":False}
-    def check_one_cell(s, t):
-        #This sub-function runs a Nash equilibrium check on one cell by comparing
-        #each player's payoff in that cell against his payoff where he plays his other option
-        #but the opponent keeps the strategy constant.
-        if p_row[s, t] >= p_row[s, ((t+1)%2)] and p_column[s, t] >= p_column[((s+1)%2), t]:
-            dic[f"{s}{t}"] = True
-    #This bit calls the check_one_cell sub-function for each of the four cells
-    for v in [0,1]:
-        for w in [0,1]:
-            check_one_cell(v, w)
-    return dic
         
-def mixed_equil_checker(p_row, p_column):
-    #This calculates the probabilities with which the players should play
-    #their pure strategies in a mixed strategy Nash equilibirum
-    #Probability for row player playing U is p, for D 1-p
-    #Probability for column player playing L is q, for R 1-q
-    numerator_p = p_column[1,1] - p_column[0,1]
-    denominator_p = p_column[0,0] - p_column[0,1] - p_column[1,0] + p_column[1,1]
-    numerator_q = p_row[1,1] - p_row[1,0]
-    denominator_q = p_row[0,0] - p_row[1,0] - p_row[0,1] + p_row[1,1]
-    return (numerator_p / denominator_p, numerator_q / denominator_q)
+        payoffs = [
+        
+            [
+                [float(ul1entry.get()), float(ur1entry.get())],
+                [float(dl1entry.get()), float(dr1entry.get())],
+            ],
+        
+            [
+                [float(ul2entry.get()), float(ur2entry.get())],
+                [float(dl2entry.get()), float(dr2entry.get())],
+            ]
+        ]
+        return payoffs
+        
+    except ValueError:
+        return None
+        
+        
+def flip(x): # Turns an index number of 0 or 1 into its opposite
+    return ((x + 1) % 2)
+        
+        
+def dominancechecker(checked1, alternative1, checked2, alternative2): # Checks a given strategy for strict or weak dominance over its alternative
+    if (checked1 == alternative1) and (checked2 == alternative2):
+        return 0
+    if (checked1 > alternative1) and (checked2 > alternative2):
+        global strict_dominance_exists 
+        strict_dominance_exists = True
+        return 2
+    if (checked1 >= alternative1) and (checked2 >= alternative2):
+        return 1
+    return 0
     
-#------------------ Displaying results on the screen -------------------
     
-def arrow_painter(dic):
-    #This function marks strategies that have been identified as dominant with an arrow
-    if dic["u"] + dic["d"] > 0:
-        if dic["u"] > 0:
-            if dic["u"] == 1:
+def arrow_painter(list):
+    
+    if list[0] + list[1] > 0: # Tests if there is any dominance among U and D (the row player's strategies)
+        if list[0] > 0:
+            if list[0] == 1:
                 i = weak_dominance_horizontal
             else: 
                 i = strict_dominance_horizontal
             x1 = 20
             y1 = 140
-        if dic["d"] > 0:
-            if dic["d"] == 1:
+        if list[1] > 0:
+            if list[1] == 1:
                 i = weak_dominance_horizontal
             else:
                 i = strict_dominance_horizontal
@@ -189,16 +131,16 @@ def arrow_painter(dic):
         arrow1 = tk.Label(image=i)
         arrow1.place(x = x1, y = y1)
         
-    if dic["l"] + dic["r"] > 0:
-        if dic["l"] >0:
-            if dic["l"] == 1:
+    if list[2] + list[3] > 0: # Tests if there is any dominance among L and R (the column player's strategies)
+        if list[2] >0:
+            if list[2] == 1:
                 j = weak_dominance_vertical
             else:
                 j = strict_dominance_vertical
             x2 = 150
             y2 = 5
         else:
-            if dic["r"] == 1:
+            if list[3] == 1:
                 j = weak_dominance_vertical
             else:
                 j = strict_dominance_vertical
@@ -206,74 +148,108 @@ def arrow_painter(dic):
             y2 = 5
         arrow2 = tk.Label(image=j)
         arrow2.place(x = x2, y = y2)
-        
-
-def pure_equil_painter(dic):
-    #This function marks cells that have been identified as 
-    #pure stragety Nash equilibria with a frame
-    for item in ["00", "01", "10", "11"]:
-        if dic[item]:
-            if item == "00":
-                x11=135
-                x22=220
-                y11=145
-                y22=210
-            elif item == "01":
-                x11=135
-                x22=220
-                y11=245
-                y22=310
-            elif item == "10":
-                x11=235
-                x22=320
-                y11=145
-                y22=210
-            else:
-                x11=235
-                x22=320
-                y11=245
-                y22=310
-            canvas.create_rectangle(x11, y11, x22, y22, fill=None, width=5, outline="blue")
-            
-
-def oddment_painter (p_here, q_here):
-    #This function displays the calculated probabilities in mixed
-    #strategy Nash equilibria
-    p_label = tk.Label(text = f"p = {round(p_here, 2)}")
-    pm1_label = tk.Label(text = f"1-p = {round(1 - p_here, 2)}")
-    q_label = tk.Label(text = f"q = {round(q_here, 2)}")
-    qm1_label = tk.Label(text = f"1-q = {round(1 - q_here, 2)}")
-    p_label.place(x = 350, y = 165)
-    pm1_label.place(x = 350, y = 265)
-    q_label.place(x = 155, y = 350)
-    qm1_label.place(x = 255, y = 350)
     
+    return
+    
+    
+def nashequil(payoffs):
+    number_of_pure_strategy_equilibria = 0
+    row_player_payoff = None
+    column_player_payoff = None
+    for j in range(2):
+        for k in range(2):
+            if (payoffs[0][k][j] >= payoffs[0][flip(k)][j]) and (payoffs[1][k][j] >= payoffs[1][k][flip(j)]):
+                    number_of_pure_strategy_equilibria += 1
+                    if (j == 0) and (k == 0):
+                        canvas.create_rectangle(135, 145, 220, 210, fill=None, width=5, outline="blue") # Up-Left
+                        row_player_payoff = payoffs[0][0][0]
+                        column_player_payoff = payoffs[1][0][0]
+                    if (j== 0) and (k == 1):
+                        canvas.create_rectangle(135, 245, 220, 310, fill=None, width=5, outline="blue") # Down-Left
+                        row_player_payoff = payoffs[0][1][0]
+                        column_player_payoff = payoffs[1][1][0]
+                    if (j == 1) and (k == 0):
+                        canvas.create_rectangle(235, 145, 320, 210, fill=None, width=5, outline="blue") # Up-Right
+                        row_player_payoff = payoffs[0][0][1]
+                        column_player_payoff = payoffs[1][0][1]
+                    if (j == 1) and (k == 1):
+                        canvas.create_rectangle(235, 245, 320, 310, fill=None, width=5, outline="blue") # Down-right
+                        row_player_payoff = payoffs[0][1][1]
+                        column_player_payoff = payoffs[1][1][1]
+                
+    return (number_of_pure_strategy_equilibria, row_player_payoff, column_player_payoff)
+    
+    
+        
+        
 def calculate():
-    #This function performs the calculations by calling a series of other
-    #functions above that do parts of the calculcations
-    try:
-        payoffs_row = get_payoffs()[0]
-        payoffs_column = get_payoffs()[1]
-        dic_of_dominance = dom_checker(payoffs_row, payoffs_column)
-        dic_of_pure_equilibria = pure_equil_checker(payoffs_row, payoffs_column)
-        if sum(dic_of_dominance.values()) > 0:
-            arrow_painter(dic_of_dominance)
-        if sum(dic_of_pure_equilibria.values()) > 0:
-            pure_equil_painter(dic_of_pure_equilibria)
-        p = mixed_equil_checker(payoffs_row, payoffs_column)[0]
-        q = mixed_equil_checker(payoffs_row, payoffs_column)[1]
-        if 0 < p < 1 and 0 < q < 1:
-            oddment_painter(p, q)
-            value_row = q * payoffs_row[0, 0] + (1 - q) * payoffs_row[1, 0]
-            value_column = p * payoffs_column[0, 0] + (1 - p) * payoffs_column[0, 1]
-            vlt = f"Value Row: {round(value_row, 2)}\nValue Column: {round(value_column, 2)}"
-            value_label = tk.Label(text = vlt)
-            value_label.place(x = 320, y = 380)
-    except TypeError:
+    payoffs = get_payoffs()
+    if payoffs == None:
+        msgwrng3 = "Please ensure all fields are filled before proceeding."
+        warning3 = tkinter.messagebox.showinfo(message=msgwrng3)
         return
+    
+    dominance_results = [0, 0, 0, 0] # Preparing list of results of dominance checks
+    # Running dominance checks for row player:
+    for i in range(2):
+        dominance_results[i] = dominancechecker(payoffs[0][i][0], payoffs[0][flip(i)][0], payoffs[0][i][1], payoffs[0][flip(i)][1])
+    
+    # Running dominance checks for column player
+    for i in range(2):
+        dominance_results[i + 2] = dominancechecker(payoffs[1][0][i], payoffs[1][0][flip(i)], payoffs[1][1][i], payoffs[1][1][flip(i)])
+        
+    # dominance_results is now a list of four variables where 2 reflects a strictly cominant strategy, 1 a weakly dominant one, and 0 a non-dominant strategy
+    # These variables correspond to the following strategies: [Row-Up, Row-Down, Column-Left, Column-Right]
+    if sum(dominance_results) > 0: # Calls the arrow_painter function (which displays dominance results graphically) if some dominance is present
+        arrow_painter(dominance_results)
+    results_of_nash_analysis = nashequil(payoffs) # Calls the function to check for Nash equilibria
+        
+    # The following part calculates the value of the game:   
+    if results_of_nash_analysis[0] == 1: # Means there is only one pure strategy Nash equilibrium
+        p = results_of_nash_analysis[1]
+        q = results_of_nash_analysis[2]
+        value_to_row_player = results_of_nash_analysis[1]
+        value_to_column_player = results_of_nash_analysis[2]
+    else:
+        # This part calculates the probabilities with which the players should play
+        # their pure strategies in a mixed strategy Nash equilibirum
+        # Probability for row player playing U is p, for D 1-p
+        # Probability for column player playing L is q, for R 1-q
+        numerator_q = payoffs[0][1][1] - payoffs[0][0][1]
+        denominator_q = payoffs[0][0][0] - payoffs[0][0][1] - payoffs[0][1][0] + payoffs[0][1][1]
+        numerator_p = payoffs[1][1][1] - payoffs[1][1][0]
+        denominator_p = payoffs[1][0][0] - payoffs[1][1][0] - payoffs[1][0][1] + payoffs[1][1][1]
+        p = numerator_p / denominator_p
+        q = numerator_q / denominator_q
+        p_label = tk.Label(text = f"p = {round(p, 2)}")
+        pm1_label = tk.Label(text = f"1-p = {round(1 - p, 2)}")
+        q_label = tk.Label(text = f"q = {round(q, 2)}")
+        qm1_label = tk.Label(text = f"1-q = {round(1 - q, 2)}")
+        p_label.place(x = 350, y = 165)
+        pm1_label.place(x = 350, y = 265)
+        q_label.place(x = 155, y = 350)
+        qm1_label.place(x = 255, y = 350)
+        value_to_row_player = payoffs[0][0][0] * p + payoffs[0][1][0] * (1 - p)
+        value_to_column_player = payoffs[1][0][0] * q + payoffs[1][0][1] * (1 - q)
+        
+    value_label = tk.Label(text = f"Values:\nRow player: {round(value_to_row_player, 2)}\nColumn player: {round(value_to_column_player, 2)}")
+    value_label.place(x = 330, y = 380)
 
-#------------------------------ Main program ---------------------------
+    return
+    
+    
 calculatebutton = tk.Button(text="Calculate", command=calculate, width=10)
-calculatebutton.place(x = 350, y = 10)
+calculatebutton.place(x = 350, y = 10)        
+        
+zerosumbutton = tk.Button(text="Zero sum", command=insert_constant_sum_payoffs, width=10)
+zerosumbutton.place(x = 20, y = 400)
+
+constantsumentry = tk.Entry(width = 10)
+constantsumentry.place(x = 110, y = 430)
+
+constantsumbutton = tk.Button(text="Constant sum", command=insert_constant_sum_payoffs, width=10)
+constantsumbutton.place(x = 110, y = 400)
+
+# ---------------------------- Main program ----------------------------
 
 window.mainloop()
